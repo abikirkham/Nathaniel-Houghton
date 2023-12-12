2d30aff4c565f64af5eb969cd675e0059f0080ac
abikirkham <144112159+abikirkham@users.noreply.github.com>
2023-12-12 21:58:32 +0000

    Update arrows.js

--- assets/javascript/arrows.js
+++ assets/javascript/arrows.js
@@ -1,1 +1,13 @@
+document.querySelector('.scroll-left').addEventListener('click', function() {
+            document.querySelector('.scroll-container').scrollBy({
+                left: -300,
+                behavior: 'smooth'
+            });
+        });
 
+        document.querySelector('.scroll-right').addEventListener('click', function() {
+            document.querySelector('.scroll-container').scrollBy({
+                left: 300,
+                behavior: 'smooth'
+            });
+        });
